c22b5b806e3cbed7fc11ec13b317c56a152f0bae
Askeliy <lexa.dementev206@mail.ru>
2024-12-01 14:53:25 +0300

    some model change

--- internal/models/question.model.go
+++ internal/models/question.model.go
@@ -1,10 +1,23 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
 
 type Question struct {
-	ID         uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	ElectionID uuid.UUID `json:"election_id" gorm:"type:uuid"`
-	Text       string    `json:"text"`
-	Choices    []string  `json:"choices" gorm:"type:text[]"`
+	ID         uuid.UUID      `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	Text       string         `json:"text"`
+	Choices    pq.StringArray `json:"choices" gorm:"type:text[]"`
+	ElectionID uuid.UUID      `json:"election_id" gorm:"type:uuid"`
+}
+
+type CreateQuestionSchema struct {
+	Text    string         `json:"text" validate:"required"`
+	Choices pq.StringArray `json:"choices"`
+}
+
+type UpdateQuestionSchema struct {
+	Text    string         `json:"text" validate:"required"`
+	Choices pq.StringArray `json:"choices"`
 }
